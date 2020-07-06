6445d93126899202c5bda2453cc9d7b366da14b6
Lean <leanjunio@live.com>
2020-07-06 16:25:49 -0400

    Updates

---
+++ precache-manifest.2c12dd6902ae52def840804b00186907.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e138c51708f101a55d4faf9d7bf25ef2",
+    "url": "/dummy/index.html"
+  },
+  {
+    "revision": "6a208d06d8ec0fc0358a",
+    "url": "/dummy/static/css/main.75df9f48.chunk.css"
+  },
+  {
+    "revision": "8374e99d959000e309c6",
+    "url": "/dummy/static/js/2.0927a14b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dummy/static/js/2.0927a14b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a208d06d8ec0fc0358a",
+    "url": "/dummy/static/js/main.4f336682.chunk.js"
+  },
+  {
+    "revision": "a16833863428926d36a4",
+    "url": "/dummy/static/js/runtime-main.14025c22.js"
+  },
+  {
+    "revision": "cf18b6297c3065e52d11afe04a8b6110",
+    "url": "/dummy/static/media/mintbean.cf18b629.png"
+  }
+]);
